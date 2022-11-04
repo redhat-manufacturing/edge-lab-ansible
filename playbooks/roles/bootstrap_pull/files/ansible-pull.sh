357e921bb39188b53244926a7031460052782a09
James Harmison <jharmison@redhat.com>
2022-11-03 20:51:44 -0400

    Attempt to correct connection

--- playbooks/roles/bootstrap_pull/files/ansible-pull.sh
+++ playbooks/roles/bootstrap_pull/files/ansible-pull.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -i
 
 set -exuo pipefail
 
 run_playbook() {
-    ansible-navigator exec -- ansible-pull -U ssh://git@github.com/redhat-manufacturing/osdu-lab-ansible.git --accept-host-key --limit "$AAP_INVENTORY_HOST" "$@" -e ansible_host=127.0.0.1
+    ansible-navigator exec -- ansible-pull -U ssh://git@github.com/redhat-manufacturing/osdu-lab-ansible.git --accept-host-key --limit "$AAP_INVENTORY_HOST" "$@"
 }
 
 for playbook in $(echo "$AAP_PLAYBOOKS" | tr ',' ' '); do
-    run_playbook "$playbook"
+    run_playbook "$playbook" "$@" ${ANSIBLE_PULL_EXTRA_ARGS}
 done
